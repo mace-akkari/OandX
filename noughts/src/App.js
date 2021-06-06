d2e6f698c3329465893bc0d88134c770c12cb9a7
Mace <maceplus@gmail.com>
2021-06-06 17:06:07 +0100

    add state and value to positions

--- noughts/src/App.js
+++ noughts/src/App.js
@@ -1,34 +1,31 @@
 import React from "react"
 import './App.css'
-import { Naught } from "./components/Naught"
-import { Cross } from "./components/Cross"
+import { Square } from "./components/Square";
 import { NAUGHT, CROSS, EMPTY } from "./lib/constants";
 
 
-function Square() {
-  return <div className="square"></div>;
-}
-
-
 function OAndX() {
-  const player = NAUGHT;
-  const positions = [
+  const [state, setState] = React.useState({
+   player: NAUGHT,
+   positions:  [
     EMPTY, EMPTY, NAUGHT,
     EMPTY, CROSS, EMPTY,
     EMPTY, NAUGHT, EMPTY
-  ];
+  ]
+})
+
   return (
     <div className="board-container">
       <div className="board">
-        <Square position={0} />
-        <Square position={1} />
-        <Square position={2} />
-        <Square position={3} />
-        <Square position={4} />
-        <Square position={5} />
-        <Square position={6} />
-        <Square position={7} />
-        <Square position={8} />
+        <Square position={0} value={state.positions[0]}/>
+        <Square position={1} value={state.positions[1]}/>
+        <Square position={2} value={state.positions[2]}/>
+        <Square position={3} value={state.positions[3]}/>
+        <Square position={4} value={state.positions[4]}/>
+        <Square position={5} value={state.positions[5]}/>
+        <Square position={6} value={state.positions[6]}/>
+        <Square position={7} value={state.positions[7]}/>
+        <Square position={8} value={state.positions[8]}/>
       </div>
     </div>
   );
