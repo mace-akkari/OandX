832f123627ff0c504139fce78dd4cd131a644917
Mace <maceplus@gmail.com>
2021-06-06 14:30:05 +0100

    add position and imports

--- noughts/src/App.js
+++ noughts/src/App.js
@@ -1,5 +1,7 @@
 import React from "react"
 import './App.css'
+import { Naught } from "./components/Naught"
+import { Cross } from "./components/Cross"
 
 
 function Square() {
@@ -10,15 +12,15 @@ function OAndX() {
   return (
     <div className="board-container">
       <div className="board">
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
+        <Square position={0} />
+        <Square position={1} />
+        <Square position={2} />
+        <Square position={3} />
+        <Square position={4} />
+        <Square position={5} />
+        <Square position={6} />
+        <Square position={7} />
+        <Square position={8} />
       </div>
     </div>
   );
