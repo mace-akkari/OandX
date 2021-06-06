136e5dcaa05f929c0f89d88ab79b632886e6b2d6
Mace <maceplus@gmail.com>
2021-06-06 18:46:05 +0100

    spelling fix

--- noughts/src/components/Result.js
+++ noughts/src/components/Result.js
@@ -3,7 +3,7 @@ import { NAUGHT, CROSS } from "../lib/constants"
 export function Result({ winner, reset }) {
   return (
     <div className="result">
-      {winner === NAUGHT && 'Circles won the game'}
+      {winner === NAUGHT && 'Naughts won the game'}
       {winner === CROSS && 'Crosses won the game'}
       {winner === 'It is a draw' && 'It is a draw'}
       <button onClick={reset}>Reset</button>
