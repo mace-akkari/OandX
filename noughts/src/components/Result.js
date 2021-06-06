e8eb4f4ee41e9bdf4ab0d71980ac974321dbb35f
Mace <maceplus@gmail.com>
2021-06-06 18:05:41 +0100

    add results

---
+++ noughts/src/components/Result.js
@@ -0,0 +1,12 @@
+import { NAUGHT, CROSS } from "../lib/constants"
+
+export function Result({ winner, reset }) {
+  return (
+    <div className="result">
+      {winner == NAUGHT && 'Circles won the game'}
+      {winner == CROSS && 'Crosses won the game'}
+      {winner == 'It is a draw' && 'It is a draw'}
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
