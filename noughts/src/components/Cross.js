aa863c9e2a8d8b81a190b2074a45f37f26665f82
Mace <maceplus@gmail.com>
2021-06-06 14:43:31 +0100

    add export to function

--- noughts/src/components/Cross.js
+++ noughts/src/components/Cross.js
@@ -1,4 +1,4 @@
-function Cross() {
+export function Cross() {
   return (
     <svg width="100" height="100" viewBox="-50 -50 100 100" className="cross">
       <line x1="-40" y1="-40" x2="40" y2="40" />  
--- noughts/src/components/Naught.js
+++ noughts/src/components/Naught.js
@@ -1,4 +1,4 @@
-function Naught() {
+export function Naught() {
   return (
     <svg width="100" height="100" viewBox="-50 -50 100 100" className="naught">
       <naught cx="0" cy="0" r="40" />  
